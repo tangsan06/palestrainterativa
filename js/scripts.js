8fc731a347759d1d0c307010118496f76c076c77
Walter Felipe Paranhos de Miranda <walterfelipeparanhos@gmail.com>
2022-09-03 13:32:30 -0300

    Add QrCode feature

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,8 @@ window.onhashchange = function() {
 
 const pages = [
     'login',
-    'home'
+    'home',
+    'qrcode'
 ];
 
 
@@ -33,4 +34,22 @@ const getPage = async (page) => {
     const html = await response.text();
     document.querySelector('#content').innerHTML = html;
     document.querySelector('#closeOffCanvas').click();
+
+
+    if(page.includes('qrcode')){
+        var scriptElement1=document.createElement('script');
+        scriptElement1.type = 'module';
+        scriptElement1.src = "js/qr-code.js";
+        document.head.appendChild(scriptElement1);
+    }
+
+
+
+    // if(page.includes('qrcode')){
+    //     if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+    //         alert('Seu navegador tem suporte');
+    //       } else {
+    //         alert('Seu navegador não tem suporte');
+    //       }
+    // }
 }
